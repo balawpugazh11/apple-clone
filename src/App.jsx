2deb9d1503e4d6c5dff0e6d5b988e0a7adb719ce
balawpugazh11 <balawpugazh@gmail.com>
2025-07-05 12:22:04 +0530

    Add back PromoBanner component for stepwise debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import AppleNavbar from "./components/Navbar";
+import PromoBanner from "./components/PromoBanner";
 
 function App() {
   return (
     <>
       <AppleNavbar />
       <div className="pt-16">
+        <PromoBanner />
         <h1 className="text-4xl font-bold text-center mt-20">Apple Clone</h1>
         <p className="text-center mt-4">If you can see this, the app is working!</p>
       </div>
