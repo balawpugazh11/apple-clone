8d18e6741506ae3a1abd2ba6fe1846dc1589a209
balawpugazh11 <balawpugazh@gmail.com>
2025-07-05 12:19:47 +0530

    Test minimal version to debug white screen issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import "./App.css";
 import AppleNavbar from "./components/Navbar";
-import PromoBanner from "./components/PromoBanner";
-import Hero from "./components/Hero";
-import Hero2 from "./components/Hero2";
-import Hero3 from "./components/Hero3";
-import Products from "./components/Products";
-import ProductBanner from "./components/ProductBanner";
-import Movieposter from "./components/MoviePoster";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <AppleNavbar />
       <div className="pt-16">
-        <PromoBanner />
-        <Hero />
-        <Hero2 />
-        <Hero3 />
-        <Products />
-        <ProductBanner />
-        <Movieposter />
-        <Footer />
+        <h1 className="text-4xl font-bold text-center mt-20">Apple Clone</h1>
+        <p className="text-center mt-4">If you can see this, the app is working!</p>
       </div>
     </>
   );
