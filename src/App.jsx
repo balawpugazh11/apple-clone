72e8342e38455493f8ce0e2ca441762c30ac4bde
balawpugazh11 <balawpugazh@gmail.com>
2025-07-05 12:28:08 +0530

    Add back ProductBanner component for stepwise debugging

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Hero2 from "./components/Hero2";
 import Hero3 from "./components/Hero3";
 import Products from "./components/Products";
+import ProductBanner from "./components/ProductBanner";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Hero2 />
         <Hero3 />
         <Products />
+        <ProductBanner />
         <h1 className="text-4xl font-bold text-center mt-20">Apple Clone</h1>
         <p className="text-center mt-4">If you can see this, the app is working!</p>
       </div>
