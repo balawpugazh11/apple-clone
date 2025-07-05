b8ce687d4ee04400ebe6ea09c916abebdb6f9dd1
balawpugazh11 <balawpugazh@gmail.com>
2025-07-05 12:24:16 +0530

    Add back Hero2 component for stepwise debugging

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import AppleNavbar from "./components/Navbar";
 import PromoBanner from "./components/PromoBanner";
 import Hero from "./components/Hero";
+import Hero2 from "./components/Hero2";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="pt-16">
         <PromoBanner />
         <Hero />
+        <Hero2 />
         <h1 className="text-4xl font-bold text-center mt-20">Apple Clone</h1>
         <p className="text-center mt-4">If you can see this, the app is working!</p>
       </div>
